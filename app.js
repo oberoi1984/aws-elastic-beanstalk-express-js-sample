1ea8ad0ace95cbeb525dda1d06d9a7c868cabb76
oberoi1984 <oberoi1984@gmail.com>
2024-08-30 01:05:14 +0530

    Change Message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Harry Oberoi!'));
+app.get('/', (req, res) => res.send('Congratulations! Your CI/CD pipeline is successfully implemented!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
